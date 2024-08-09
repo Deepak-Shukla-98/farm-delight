64abbfd55bbffa6cbe61122f5603e4c857b0770f
vanitha <vanitha.bolle@gmail.com>
2024-08-09 18:28:49 +0530

    track orders

--- src/app/(pages)/(protected)/history/page.tsx
+++ src/app/(pages)/(protected)/history/page.tsx
@@ -19,84 +19,17 @@ function Page() {
         <h2 className="font-manrope font-bold text-3xl lead-10 text-black mb-9">
           Order History
         </h2>
-
-        {/* <div className="flex sm:flex-col lg:flex-row sm:items-center justify-end">
-          <ul className="flex max-sm:flex-col sm:items-center gap-x-14 gap-y-3">
-            <li className="font-medium text-lg leading-8 cursor-pointer text-indigo-600 transition-all duration-500 hover:text-indigo-600">
-              All Order
-            </li>
-            <li className="font-medium text-lg leading-8 cursor-pointer text-black transition-all duration-500 hover:text-indigo-600">
-              Summary
-            </li>
-            <li className="font-medium text-lg leading-8 cursor-pointer text-black transition-all duration-500 hover:text-indigo-600">
-              Completed
-            </li>
-            <li className="font-medium text-lg leading-8 cursor-pointer text-black transition-all duration-500 hover:text-indigo-600">
-              Cancelled
-            </li>
-          </ul>
-          <div className="flex max-sm:flex-col items-center justify-end gap-2 max-lg:mt-5">
-            <div className="flex rounded-full py-3 px-4 border border-gray-300 relative">
-              <svg
-                className="relative "
-                width="18"
-                height="20"
-                viewBox="0 0 18 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1.5 7.75H16.5M11.9213 11.875H11.928M11.9212 14.125H11.9279M9.14676 11.875H9.1535M9.14676 14.125H9.1535M6.37088 11.875H6.37762M6.37088 14.125H6.37762M5.25 4.75V1.75M12.75 4.75V1.75M7.5 18.25H10.5C13.3284 18.25 14.7426 18.25 15.6213 17.3713C16.5 16.4926 16.5 15.0784 16.5 12.25V9.25C16.5 6.42157 16.5 5.00736 15.6213 4.12868C14.7426 3.25 13.3284 3.25 10.5 3.25H7.5C4.67157 3.25 3.25736 3.25 2.37868 4.12868C1.5 5.00736 1.5 6.42157 1.5 9.25V12.25C1.5 15.0784 1.5 16.4926 2.37868 17.3713C3.25736 18.25 4.67157 18.25 7.5 18.25Z"
-                  stroke="#111827"
-                  stroke-width="1.8"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-              <input
-                type="text"
-                name="from-dt"
-                id="from-dt"
-                className="font-semibold px-2 text-sm text-gray-900 outline-0 appearance-none flex flex-row-reverse cursor-pointer w-28 placeholder-gray-900"
-                placeholder="11-01-2023"
-              />
-            </div>
-            <p className="font-medium text-lg leading-8 text-black">To</p>
-            <div className="flex rounded-full py-3 px-4 border border-gray-300 relative">
-              <svg
-                className="relative "
-                width="18"
-                height="20"
-                viewBox="0 0 18 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M1.5 7.75H16.5M11.9213 11.875H11.928M11.9212 14.125H11.9279M9.14676 11.875H9.1535M9.14676 14.125H9.1535M6.37088 11.875H6.37762M6.37088 14.125H6.37762M5.25 4.75V1.75M12.75 4.75V1.75M7.5 18.25H10.5C13.3284 18.25 14.7426 18.25 15.6213 17.3713C16.5 16.4926 16.5 15.0784 16.5 12.25V9.25C16.5 6.42157 16.5 5.00736 15.6213 4.12868C14.7426 3.25 13.3284 3.25 10.5 3.25H7.5C4.67157 3.25 3.25736 3.25 2.37868 4.12868C1.5 5.00736 1.5 6.42157 1.5 9.25V12.25C1.5 15.0784 1.5 16.4926 2.37868 17.3713C3.25736 18.25 4.67157 18.25 7.5 18.25Z"
-                  stroke="#111827"
-                  stroke-width="1.8"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                />
-              </svg>
-              <input
-                type="text"
-                name="to-dt"
-                id="to-dt"
-                className="font-semibold px-2 text-sm text-gray-900 outline-0 appearance-none flex flex-row-reverse cursor-pointer w-28 placeholder-gray-900"
-                placeholder="11-01-2023"
-              />
-            </div>
-          </div>
-        </div> */}
         {orders.map((d: any) => (
           <div className="mb-6 main-box border border-gray-200 rounded-xl pt-6 max-w-xl max-lg:mx-auto lg:max-w-full">
             <div className="flex flex-col lg:flex-row lg:items-center justify-between px-6 pb-6 border-b border-gray-200">
               <div className="data">
-                <p className="font-semibold text-base leading-7 text-black">
+                <Link
+                  href={`/track-order?id=${d.id}`}
+                  className="font-semibold text-base leading-7 text-black"
+                >
                   Order Id:{" "}
                   <span className="text-indigo-600 font-medium">#{d.id}</span>
-                </p>
+                </Link>
                 <p className="font-semibold text-base leading-7 text-black mt-4">
                   Order Payment :{" "}
                   <span className="text-gray-400 font-medium">
--- src/app/(pages)/(protected)/order-placed/page.tsx
+++ src/app/(pages)/(protected)/order-placed/page.tsx
@@ -1,20 +1,30 @@
 "use client";
-import { getOrdersById } from "@/components/services/axios";
+import { getOrdersById, updateAdminOrders } from "@/components/services/axios";
 import { useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import moment from "moment";
+import Link from "next/link";
 
 function Page() {
   const searchParams = useSearchParams();
+  const id = searchParams.get("id");
   const [orders, setOrders] = useState<any>({ orderItems: [] });
   const getData = async () => {
-    let [data] = await getOrdersById({ id: searchParams.get("id") });
+    let [data] = await getOrdersById({ id });
     if (!!data) setOrders(data);
   };
   useEffect(() => {
     getData();
   }, []);
-  const total = orders.orderItems.reduce((a: any, s: any) => a + s?.price, 0);
+  const total = orders.orderItems.reduce(
+    (a: any, s: any) => a + s?.price * s?.quantity,
+    0
+  );
+  const handleCancel = async () => {
+    let { orderItems, ...rest } = orders;
+    await updateAdminOrders({ ...rest, status: "CANCELLED" });
+    getData();
+  };
 
   return (
     <section className="py-24 relative">
@@ -42,9 +52,12 @@ function Page() {
                 </span>
               </p>
             </div>
-            <button className="rounded-full py-3 px-7 font-semibold text-sm leading-7 text-white bg-indigo-600 max-lg:mt-5 shadow-sm shadow-transparent transition-all duration-500 hover:bg-indigo-700 hover:shadow-indigo-400">
+            <Link
+              href={`/track-order?id=${id}`}
+              className="rounded-full py-3 px-7 font-semibold text-sm leading-7 text-white bg-indigo-600 max-lg:mt-5 shadow-sm shadow-transparent transition-all duration-500 hover:bg-indigo-700 hover:shadow-indigo-400"
+            >
               Track Your Order
-            </button>
+            </Link>
           </div>
           {orders?.orderItems.map((d: any) => (
             <div className="w-full px-3 min-[400px]:px-6">
@@ -91,7 +104,7 @@ function Page() {
                             Status
                           </p>
                           <p className="font-medium text-sm leading-6 whitespace-nowrap py-0.5 px-3 rounded-full lg:mt-3 bg-emerald-50 text-emerald-600">
-                            Order Placed
+                            {orders.status}
                           </p>
                         </div>
                       </div>
@@ -112,30 +125,35 @@ function Page() {
             </div>
           ))}
           <div className="w-full border-t border-gray-200 px-6 flex flex-col lg:flex-row items-center justify-between ">
-            <div className="flex flex-col sm:flex-row items-center max-lg:border-b border-gray-200">
-              <button className="flex outline-0 py-6 sm:pr-6  sm:border-r border-gray-200 whitespace-nowrap gap-2 items-center justify-center font-semibold group text-lg text-black bg-white transition-all duration-500 hover:text-indigo-600">
-                <svg
-                  className="stroke-black transition-all duration-500 group-hover:stroke-indigo-600"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="22"
-                  height="22"
-                  viewBox="0 0 22 22"
-                  fill="none"
+            {orders.status === "PENDING" && (
+              <div className="flex flex-col sm:flex-row items-center max-lg:border-b border-gray-200">
+                <button
+                  className="flex outline-0 py-6 sm:pr-6  sm:border-r border-gray-200 whitespace-nowrap gap-2 items-center justify-center font-semibold group text-lg text-black bg-white transition-all duration-500 hover:text-indigo-600"
+                  onClick={handleCancel}
                 >
-                  <path
-                    d="M5.5 5.5L16.5 16.5M16.5 5.5L5.5 16.5"
-                    stroke=""
-                    stroke-width="1.6"
-                    stroke-linecap="round"
-                  />
-                </svg>
-                Cancel Order
-              </button>
-              {/* <p className="font-medium text-lg text-gray-900 pl-6 py-3 max-lg:text-center">
+                  <svg
+                    className="stroke-black transition-all duration-500 group-hover:stroke-indigo-600"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="22"
+                    height="22"
+                    viewBox="0 0 22 22"
+                    fill="none"
+                  >
+                    <path
+                      d="M5.5 5.5L16.5 16.5M16.5 5.5L5.5 16.5"
+                      stroke=""
+                      stroke-width="1.6"
+                      stroke-linecap="round"
+                    />
+                  </svg>
+                  Cancel Order
+                </button>
+                {/* <p className="font-medium text-lg text-gray-900 pl-6 py-3 max-lg:text-center">
                 Paid using Credit Card{" "}
                 <span className="text-gray-500">ending with 8822</span>
               </p> */}
-            </div>
+              </div>
+            )}
             <p className="font-semibold text-lg text-black py-6">
               Total Price: <span className="text-indigo-600"> ${total}</span>
             </p>
